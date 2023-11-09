4b138ffefaa5c68ebce7795eaca90ce26a12cc5f
Yash <thisisyashs@gmail.com>
2023-11-09 17:36:17 +0530

    jest and tailwind config integration

--- packages/tailwindconfig/index.ts
+++ packages/tailwindconfig/index.ts
@@ -3,11 +3,31 @@ module.exports = {
   content: [],
   theme: {
     extend: {
+      colors: {
+        brand: {
+          "50": "#f0fcfc",
+          "100": "#dff7f7",
+          "200": "#b2eaed",
+          "300": "#88dee3",
+          "400": "#3ec7cf",
+          "500": "#00adbb",
+          "600": "#0095a8",
+          "700": "#00738c",
+          "800": "#005470",
+          "900": "#003a54",
+          "950": "#002036",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+  },
+  variants: {
+    extend: {
+      animation: ["group-hover", "animate-spin"],
     },
   },
   plugins: [require("flowbite/plugin")],
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,15 +1,60 @@
-type ButtonProps = {
-  text: string;
-  onClick: () => void;
-};
+"use client";
 
-export const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
+import classNames from "classnames";
+import { ReactNode } from "react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+
+export interface ButtonProps
+  extends Omit<React.HTMLProps<HTMLButtonElement>, "size"> {
+  children?: ReactNode;
+  onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+  type?: "primary" | "default" | "dashed" | "text" | "link";
+  size?: "lg" | "md" | "sm";
+  loading?: boolean;
+  icon?: ReactNode;
+  disabled?: boolean;
+  action?: "submit" | "reset" | "button";
+}
+
+export function Button({
+  children,
+  type = "default",
+  size = "md",
+  loading = false,
+  icon,
+  disabled = false,
+  action = "button",
+  onClick,
+  ...restProps
+}: ButtonProps) {
   return (
     <button
-      className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg transition duration-300 ease-in-out"
+      className={classNames(
+        "flex justify-center items-center gap-1 rounded-md",
+        {
+          "text-white bg-brand-500 hover:bg-brand-600": type === "primary",
+          "border border-brand-600 hover:border-brand-700 border-dashed text-brand-600 hover:text-brand-700":
+            type === "dashed",
+          " border border-brand-600 hover:border-brand-700 text-brand-600 hover:text-brand-700":
+            type === "default",
+          "text-brand-600 hover:text-brand-700": type === "link",
+          "text-gray-700 hover:text-gray-800": type === "text",
+          "h-6 px-3 py-1 text-sm": size === "sm",
+          "h-8 px-4 py-2 text-md": size === "md",
+          "h-12 px-6 py-3 text-lg": size === "lg",
+          "text-gray-700 bg-gray-300 hover:bg-gray-400 border-gray-700   cursor-not-allowed":
+            disabled || loading,
+        }
+      )}
+      type={action}
+      {...restProps}
+      disabled={disabled || loading}
       onClick={onClick}
     >
-      {text}
+      <span className={classNames("inline-flex", { "animate-spin": loading })}>
+        {loading ? <AiOutlineLoading3Quarters /> : icon}
+      </span>
+      <span>{children}</span>
     </button>
   );
-};
+}
