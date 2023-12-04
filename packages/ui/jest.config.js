9d0e6219cbdbacb0c2ef072b6981fe6ad3037be6
Yash <thisisyashs@gmail.com>
2023-12-04 18:21:38 +0530

    Jest configuration cleaned

--- packages/ui/jest.config.js
+++ packages/ui/jest.config.js
@@ -1,15 +1,6 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jest-environment-jsdom",
-
-  //Globals is deprecated . Changed it to  transform.
-  // globals: {
-  //   "ts-jest": {
-  //     tsconfig: "tsconfig.jest.json",
-  //   },
-  // },
-  transform: {
-    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.jest.json" }],
-  },
+//Just importing the config from jsconfig workspace and exporting it
+const sharedConfig = require("jestconfig");
+const config = {
+  ...sharedConfig,
 };
+module.exports = config;
